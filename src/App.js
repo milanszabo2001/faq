97abc8cf174a545898c8c085ef01776cd56b80c6
Your Name <you@example.com>
2022-10-07 10:39:34 +0200

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {questions} from './data';
+import { useState } from 'react';
+
 
 function App() {
+  const [faq,setFaq]=useState(questions)
+  const [showInfo,setShowInfo]=useState(false)
+  const [selId,setSelId]=useState(null)
+  const handlePLus=(e)=>{
+    console.log(e.target.id)
+    setSelId(e.target.id)
+    if(showInfo==false)
+    setShowInfo(true)
+    if(showInfo==true)
+    setShowInfo(false)
+  }
+  console.log(faq)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container text-center'>
+      <h2>FAQ</h2>
+      {faq.map(obj=>(
+        <div className='kerdesek' key={obj.id}>
+          <h4>{obj.title}</h4>
+          <button id={obj.id} onClick={handlePLus}>{showInfo && obj.id==selId ? "-" : "+"}</button>
+          {showInfo && obj.id==selId && <div>{obj.info}</div>}
+        </div>
+      ))}
     </div>
+
   );
 }
 
